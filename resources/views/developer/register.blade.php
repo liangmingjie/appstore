c36877a3ce30ae4446f781d4fb5830b278830afa
Dxtan <1073373598@qq.com>
2015-08-29 00:13:51 +0800

    开发者注册html
    开发者注册html

--- resources/views/developer/register.blade.php
+++ resources/views/developer/register.blade.php
@@ -1,5 +1,44 @@
 @extends('app_dev')
 
 @section('content')
-regeister
+    <h2 class="text-center">基本信息</h2>
+    <hr>
+
+    {!! Form::open(['url' => 'developer','class' => 'container text-center ']) !!}
+
+    <div class="form-group">
+        {!! Form::label('user_name','用户名:', ['class' => '']) !!}
+        {!! Form::text('user_name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('email','邮箱:', ['class' => '']) !!}
+        {!! Form::text('email', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('real_name','真实姓名:', ['class' => 'control-label']) !!}
+        {!! Form::text('real_name', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('telephone','联系电话:', ['class' => 'control-label']) !!}
+        {!! Form::text('telephone', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('id_card','身份证号码:', ['class' => 'control-label']) !!}
+        {!! Form::text('id_card', null, ['class' => 'form-control']) !!}
+    </div>
+
+    <div class="form-group">
+        {!! Form::label('id_card_image','身份证扫描件:', ['class' => 'control-label']) !!}
+        {!! Form::image('http://appstore.com/images/app/logo-2.png') !!}
+        {!! Form::file('id_card_image') !!}
+    </div>
+
+    {!! Form::submit('注册',['class'=> 'btn'])  !!}
+
+    {!! Form::close() !!}
+
 @endsection
