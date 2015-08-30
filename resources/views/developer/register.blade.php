6d312b37d0976e2772ac01305d3419e2b02291cb
Dxtan <1073373598@qq.com>
2015-08-30 15:00:50 +0800

    开发者注册界面

--- resources/views/developer/register.blade.php
+++ resources/views/developer/register.blade.php
@@ -1,44 +1,85 @@
 @extends('app_dev')
 
 @section('content')
-    <h2 class="text-center">基本信息</h2>
-    <hr>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading text-center">基本信息</div>
+                <div class="panel-body">
+                    @if (count($errors) > 0)
+                        <div class="alert alert-danger">
+                            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    @endif
 
-    {!! Form::open(['url' => 'developer','class' => 'container text-center ']) !!}
+                    {!! Form::open(['url' => 'developer','class' => 'form-horizontal','role' => 'form']) !!}
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-    <div class="form-group">
-        {!! Form::label('user_name','用户名:', ['class' => '']) !!}
-        {!! Form::text('user_name', null, ['class' => 'form-control']) !!}
+                        <div class="form-group">
+                            {!! Form::label('user_name','用户名:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('user_name', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('email','邮箱:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('email', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('real_name','真实姓名:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('real_name', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('telephone','联系电话:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('telephone', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('id_card','身份证号码:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('id_card', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('id_card_image','身份证正面照:', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::file('id_card_image', null, ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            {!! Form::label('id_card_image','示例:', ['class' => 'col-md-4 control-label']) !!}
+                            {!! Form::image("/images/register/idcard-sample1.png", null, ['class' => 'col-md-6']) !!}
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Submit
+                                </button>
+                            </div>
+                        </div>
+                    {!! Form::close() !!}
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="form-group">
-        {!! Form::label('email','邮箱:', ['class' => '']) !!}
-        {!! Form::text('email', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('real_name','真实姓名:', ['class' => 'control-label']) !!}
-        {!! Form::text('real_name', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('telephone','联系电话:', ['class' => 'control-label']) !!}
-        {!! Form::text('telephone', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('id_card','身份证号码:', ['class' => 'control-label']) !!}
-        {!! Form::text('id_card', null, ['class' => 'form-control']) !!}
-    </div>
-
-    <div class="form-group">
-        {!! Form::label('id_card_image','身份证扫描件:', ['class' => 'control-label']) !!}
-        {!! Form::image('http://appstore.com/images/app/logo-2.png') !!}
-        {!! Form::file('id_card_image') !!}
-    </div>
-
-    {!! Form::submit('注册',['class'=> 'btn'])  !!}
-
-    {!! Form::close() !!}
+</div>
 
 @endsection
